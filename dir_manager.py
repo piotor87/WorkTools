aa14507e8d3138984c93ecbd8253a301919400cc
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-01-30 09:56:59 +0200

    First Commit

---
+++ dir_manager.py
@@ -0,0 +1,9 @@
+import os
+
+def make_sure_path_exists(path):
+    import errno
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise                
