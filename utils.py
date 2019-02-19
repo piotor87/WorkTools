21607e2663d75bc8dff2caa5b1f941a8f181b588
piotor87 <pietro.dellabriottaparolo@gmail.com>
2019-02-19 19:30:53 +0200

    added natural_sort

--- utils.py
+++ utils.py
@@ -204,3 +204,9 @@ def tmp_bash(cmd):
     scriptFile.file.close()
     subprocess.check_call(scriptFile.name)
     
+def natural_sort(l):
+    import re
+    convert = lambda text: int(text) if text.isdigit() else text.lower() 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(l, key = alphanum_key)
+
